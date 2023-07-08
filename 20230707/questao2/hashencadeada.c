c5c30397e8c95afd72ccc47cd208f452a98ebce2
Sarah Portela Salehi <sps.snf21@uea.edu.br>
2023-07-07 22:45:49 -0400

    hash lista encadeada

---
+++ 20230707/questao2/hashencadeada.c
@@ -0,0 +1,59 @@
+#include "hashencadeada.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void inicializaHash(TB_HASH tab) {
+    int i;
+    for (i = 0; i < N; i++) {
+        tab[i] = NULL;
+    }
+}
+
+int m_hash(int cod_pac) {
+    return (cod_pac % N);
+}
+
+int inserir(TB_HASH tab, Paciente pac) {
+    int h = m_hash(pac.cod_pac);
+    Elemento* novo = (Elemento*)malloc(sizeof(Elemento));
+    if (novo == NULL) {
+        return 0; // Falha ao alocar memória
+    }
+    novo->pac = pac;
+    novo->proximo = tab[h];
+    tab[h] = novo;
+    return 1; // Paciente inserido com sucesso
+}
+
+Paciente* busca(TB_HASH tab, int cod_pac) {
+    int h = m_hash(cod_pac);
+    Elemento* atual = tab[h];
+    while (atual != NULL) {
+        if (atual->pac.cod_pac == cod_pac) {
+            return &(atual->pac); // Paciente encontrado na tabela
+        }
+        atual = atual->proximo;
+    }
+    return NULL; // Paciente não encontrado
+}
+
+void imprimirHash(TB_HASH tab) {
+    int i;
+    for (i = 0; i < N; i++) {
+        printf("Indice %d:\n", i);
+        Elemento* atual = tab[i];
+        while (atual != NULL) {
+            printf("Codigo: %d\n", atual->pac.cod_pac);
+            printf("Nome: %s\n", atual->pac.nome);
+            printf("CPF: %s\n", atual->pac.cpf);
+            printf("Idade: %d\n", atual->pac.idade);
+            printf("--------------------\n");
+            atual = atual->proximo;
+        }
+        if (tab[i] == NULL) {
+            printf("Nenhum paciente neste indice.\n");
+            printf("--------------------\n");
+        }
+    }
+}
+
---
+++ 20230707/questao2/main.c
@@ -0,0 +1,39 @@
+#include "hashencadeada.h"
+#include <stdio.h>
+
+int main() {
+    TB_HASH tabela;
+    Paciente *pr;
+    inicializaHash(tabela);
+
+    // Exemplo de inserção de pacientes
+    Paciente p1 = { 123, "Joao", "12345678901", 25 };
+    Paciente p2 = { 456, "Maria", "98765432101", 32 };
+    Paciente p3 = { 123, "Carina", "90769432101", 32 };
+
+    inserir(tabela, p1);
+    inserir(tabela, p2);
+    inserir(tabela, p3);
+
+    // Imprimir a tabela
+    printf("Tabela Hash:\n");
+    imprimirHash(tabela);
+
+    // Exemplo de busca de paciente
+    pr = busca(tabela, 456);
+    printf("\nAchou! Nome: %s", pr->nome);
+
+    int cod_pac_busca = 400;
+    Paciente* paciente_encontrado = busca(tabela, cod_pac_busca);
+    if (paciente_encontrado != NULL) {
+        printf("Paciente encontrado:\n");
+        printf("Codigo: %d\n", paciente_encontrado->cod_pac);
+        printf("Nome: %s\n", paciente_encontrado->nome);
+        printf("CPF: %s\n", paciente_encontrado->cpf);
+        printf("Idade: %d\n", paciente_encontrado->idade);
+    } else {
+        printf("\nPaciente nao encontrado na tabela.(codigo: %d)\n", cod_pac_busca);
+    }
+
+    return 0;
+}
